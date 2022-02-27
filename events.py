3afde2c4e90f5c539a1c3d13524e142e1f8fe842
Ken <veryshj123@gmail.com>
2022-02-27 20:32:25 +0900

    Add

---
+++ events.py
@@ -0,0 +1,43 @@
+from typing import Literal, Any, ContextManager
+from dataclasses import dataclass
+from collections import defaultdict
+from contextlib import contextmanager
+
+
+@dataclass
+class EventRecord:
+    type: Literal['subscribe', 'publish']
+    event: str
+    args: tuple[Any]
+
+    def __str__(self) -> str:
+        args_str = ', '.join(str(_) for _ in self.args)
+        return f'{self.type} {self.event}: ({args_str})'
+
+
+class Events:
+    def __init__(self):
+        self._events = defaultdict(list)
+        self.records = None  # type: list | None
+
+    def subscribe(self, event: str, callback: callable):
+        if self.records is not None:
+            self.records.append(EventRecord(type='subscribe', event=event, args=(callback.__qualname__,)))
+        self._events[event].append(callback)
+
+    def publish(self, event, *args, **kwargs):
+        if self.records is not None:
+            self.records.append(EventRecord(
+                type='publish', event=event, args=tuple(args) + tuple(_ for _ in kwargs.values())))
+        for callback in self._events[event]:
+            callback(*args, **kwargs)
+
+
+@contextmanager
+def capture_events(events: Events) -> ContextManager[list[EventRecord]]:
+    records = []
+    events.records = records
+    try:
+        yield records
+    finally:
+        events.records = None
